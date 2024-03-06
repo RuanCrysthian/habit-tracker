36762e639ae628159913210f47e77a45637e6231
Ruan Ferraz <ruanlima155@gmail.com>
2024-03-06 17:46:20 -0300

    feat: Role and User repositories

---
+++ habit-tracker/src/main/java/com/rfdev/habittracker/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.rfdev.habittracker.repositories;
+
+import com.rfdev.habittracker.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+}
